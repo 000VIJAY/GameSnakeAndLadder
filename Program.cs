674d9fd7ac82adaca81f9498957add8ab5349e4c
000VIJAY <vijay893689@gmail.com>
2022-06-16 17:30:29 +0530

    added player are play or no play

--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
 ﻿Console.WriteLine("Welcome to game of snake and ladder");
 int playerOnePosition = 0;
-Random rnd = new Random(); 
-int dieNum = rnd.Next(1, 7); 
+Random rnd = new Random();
+int dieNum = rnd.Next(1, 7);
 int playerOneCurrentPosition = dieNum + playerOnePosition;
-Console.WriteLine("position : " + playerOneCurrentPosition);
+Console.WriteLine("playerOneCurrentPosition : " + playerOneCurrentPosition);
+
+Random random = new Random();
+int snakeLadder = random.Next(1, 4);
+Console.WriteLine("snakeLadder : " + snakeLadder);
+    if (snakeLadder == 1)
+    { Console.WriteLine("snake");
+    int playerOneNewPosition = playerOneCurrentPosition - snakeLadder;
+    Console.WriteLine("position : " + playerOneNewPosition);
+}
+else if(snakeLadder == 2)
+{
+    Console.WriteLine("ladder");
+    int playerOneNewPosition = playerOneCurrentPosition + snakeLadder;
+    Console.WriteLine("position : " + playerOneNewPosition);
+
+}
+else
+    {
+    Console.WriteLine("no play");
+    };
