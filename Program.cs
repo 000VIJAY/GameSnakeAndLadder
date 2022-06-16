70f950c65b34f6b8d34351b005054a28b81fbdc6
000VIJAY <vijay893689@gmail.com>
2022-06-16 16:31:11 +0530

    added player position in snake and ladder game after random die number

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿Console.WriteLine("Welcome to game of snake and ladder");
-int playerOnePosition = 0; 
-Console.WriteLine("position : " + playerOnePosition);
+int playerOnePosition = 0;
+Random rnd = new Random(); 
+int dieNum = rnd.Next(1, 7); 
+int playerOneCurrentPosition = dieNum + playerOnePosition;
+Console.WriteLine("position : " + playerOneCurrentPosition);
